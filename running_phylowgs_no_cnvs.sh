c2be999ab23ca4c841a5f3edf0a63b4ea0a8cf62
karini925 <karin.isaev@gmail.com>
2019-08-15 13:59:17 -0400

    new

---
+++ running_phylowgs_no_cnvs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH -N 1 # Ensure that all cores are on one machine
+#SBATCH -p himem
+#SBATCH --mem=61440M
+#SBATCH -t 5-00:00 # Runtime in D-HH:MM
+#SBATCH -J phylowgs
+	
+module load python2
+
+#test_ssm.txt just has 1000 of the total unique mutations 
+#phylowgs run time is proportional to number of mutations 
+#less 2019-07-16_ssm_data_RAP_WGS_input.txt | head -2500 > test_ssm.txt
+
+python2 /cluster/home/kisaev/phylowgs/multievolve.py --num-chains 4 --ssms test_ssm.txt --cnvs cnv_data.txt --burnin-samples 1 --mcmc-samples 1
+
