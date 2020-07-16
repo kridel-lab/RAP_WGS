cda0fb8dee9d38c55f87d19e57e5b8029a452dca
karini925 <karin.isaev@gmail.com>
2020-07-16 11:11:44 -0400

    treeomics vcf prep updated smaller list

--- Part_4_Phylogeny_analysis/Treeomics/treeomics_001_input_VCF_files.sh
+++ Part_4_Phylogeny_analysis/Treeomics/treeomics_001_input_VCF_files.sh
@@ -7,7 +7,7 @@
 #SBATCH -J merging_vars
 #SBATCH --array=0-19 # job array index
 
-#this follow the script 
+#this follow the script
 #RAP_WGS/Part_1_Somatic_variant_calling/Merged_Variant_Callers/Merged_005_prepare_bed_files_to_fillter_VCFs_by_soft_filters.R
 
 module load java/8  #8
@@ -27,12 +27,12 @@ echo ${names[${SLURM_ARRAY_TASK_ID}]}
 pat=${names[${SLURM_ARRAY_TASK_ID}]}
 
 vcf_file=MUTECT2_RESULTS/mutect2_filtered/${pat}_mutect2_selectvariants.vcf.gz.normalized.vcf.gz
-variant_file=merged_MUTECT2_STRELKA/merged_variants_vcfs/vcf_summary_text/2019-11-14_TREEOMICS_INPUT_MUTS_int_with_VCFs.bed
+variant_file=merged_MUTECT2_STRELKA/merged_variants_vcfs/vcf_summary_text/2020-07-16_TREEOMICS_INPUT_MUTS_int_with_VCFs.bed
 
 #first make tbi for vcf
-#tabix -p vcf $vcf_file 
+#tabix -p vcf $vcf_file
 
-#extract filtered variants from mutect2 VCF files so that Treeomics has less variants to analyze and hopefully 
-tabix -fhB $vcf_file $variant_file > treeomics/src/input/STRELKA_VCF_INPUTS/${pat}_mutect2_treeomics_input.vcf
-bcftools view -s $pat treeomics/src/input/STRELKA_VCF_INPUTS/${pat}_mutect2_treeomics_input.vcf > treeomics/src/input/STRELKA_VCF_INPUTS/${pat}_mutect2_treeomics_input_just_tumour.vcf
-rm treeomics/src/input/STRELKA_VCF_INPUTS/${pat}_mutect2_treeomics_input.vcf
+#extract filtered variants from mutect2 VCF files so that Treeomics has less variants to analyze and hopefully
+tabix -fhB $vcf_file $variant_file > treeomics/src/input/mutect2_strelka_vcf_pcg_only/${pat}_mutect2_treeomics_input.vcf
+bcftools view -s $pat treeomics/src/input/mutect2_strelka_vcf_pcg_only/${pat}_mutect2_treeomics_input.vcf > treeomics/src/input/mutect2_strelka_vcf_pcg_only/${pat}_mutect2_treeomics_input_just_tumour.vcf
+rm treeomics/src/input/mutect2_strelka_vcf_pcg_only/${pat}_mutect2_treeomics_input.vcf
