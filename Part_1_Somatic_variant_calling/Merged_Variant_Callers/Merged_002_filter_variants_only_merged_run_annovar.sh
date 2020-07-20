6a9a9c258d48229eb2d6dc5c8e0bb861235a37eb
karini925 <karin.isaev@gmail.com>
2020-07-20 14:59:46 -0400

    changed vcf input file to be the normalized one and reduced memory job requirments

--- Part_1_Somatic_variant_calling/Merged_Variant_Callers/Merged_002_filter_variants_only_merged_run_annovar.sh
+++ Part_1_Somatic_variant_calling/Merged_Variant_Callers/Merged_002_filter_variants_only_merged_run_annovar.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #
 #SBATCH -N 1 # Ensure that all cores are on one machine
-#SBATCH -p himem
-#SBATCH --mem=61440M
 #SBATCH -t 5-00:00 # Runtime in D-HH:MM
 #SBATCH -J merging_vars
 #SBATCH --array=0-19 # job array index
@@ -34,7 +32,7 @@ names=($(cat /cluster/projects/kridelgroup/RAP_ANALYSIS/patient_ids.txt))
 echo ${names[${SLURM_ARRAY_TASK_ID}]}
 pat=${names[${SLURM_ARRAY_TASK_ID}]}
 
-vcf_file=MUTECT2_RESULTS/mutect2_filtered/${pat}_mutect2_selectvariants.vcf.gz
+vcf_file=MUTECT2_RESULTS/mutect2_filtered/${pat}_mutect2_selectvariants.vcf.gz.normalized.vcf.gz
 variant_file=/cluster/projects/kridelgroup/RAP_ANALYSIS/merged_MUTECT2_STRELKA/_${pat}_merged_mutations.bed
 
 tabix -fhB $vcf_file $variant_file > merged_MUTECT2_STRELKA/merged_variants_vcfs/${pat}_merged_variants.vcf
