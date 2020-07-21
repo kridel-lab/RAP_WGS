2b8bfacba4c150e26d7b51b2c7ba9aee9c98fbd9
karini925 <karin.isaev@gmail.com>
2020-07-21 13:24:40 -0400

    made sure MYC is mediastinum still here rerun

---
+++ Part_1_Somatic_variant_calling/Merged_Variant_Callers/Merged_003B_remove_extra_AF_column.sh
@@ -0,0 +1,7 @@
+module load vt
+
+cd /cluster/projects/kridelgroup/RAP_ANALYSIS/merged_MUTECT2_STRELKA/merged_variants_vcfs/vcfs_annovar_annotated
+
+for filename in *.hg19_multianno.vcf
+   do vt rminfo $filename -t AF -o no_info_AF_$filename
+done
