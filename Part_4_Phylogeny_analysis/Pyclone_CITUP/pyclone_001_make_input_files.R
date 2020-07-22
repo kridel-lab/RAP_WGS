bac6bb6a3e8c1c3b854caf66c5496ba0d2e33234
karini925 <karin.isaev@gmail.com>
2020-07-22 10:21:25 -0400

    final input files for pyclone create

--- Part_4_Phylogeny_analysis/Pyclone_CITUP/pyclone_001_make_input_files.R
+++ Part_4_Phylogeny_analysis/Pyclone_CITUP/pyclone_001_make_input_files.R
@@ -12,7 +12,7 @@ date = Sys.Date()
 print(date)
 
 options(stringsAsFactors=F)
-setwd("/cluster/projects/kridelgroup/RAP_ANALYSIS/merged_MUTECT2_STRELKA/merged_variants_vcfs/vcf_summary_text")
+setwd("/cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/Pyclone")
 
 #load libraries
 packages <- c("dplyr", "readr", "ggplot2", "vcfR", "tidyr", "mclust", "data.table",
@@ -23,37 +23,11 @@ library(RColorBrewer)
 library(openxlsx)
 library(plotly)
 
-#2. Sample summary
-dna = fread("RAP_DNA.txt") ; dna=dna[,1:3] ; colnames(dna)[2] = "barcode"; dna$barcode = as.numeric(dna$barcode)
-biops = fread("RAP_FFbiopsies_extracted.txt" ); biops = biops[,1:6] ; colnames(biops)[4] = "barcode"
-dna = merge(dna, biops, by="barcode")
-colnames(dna)[2] = "Indiv"
-colnames(dna)[7] = "Tissue_Site"
-colnames(dna)[8] = "Specimen_Type"
-dna$Specimen_Type = "FT"
-
-dna = as.data.table(filter(dna, Indiv %in% muts$Indiv))
-ffpe = as.data.table(matrix(ncol=ncol(dna), nrow=3))
-colnames(ffpe) = colnames(dna)
-ffpe = as.data.frame(ffpe)
-
-#ffpe$Indiv = unique(muts$vcf_sample[which(!(muts$vcf_sample %in% dna$Indiv))])
-ffpe$Indiv = c("LY_RAP_0003_Dia_FoT_05", "LY_RAP_0003_Dia_FoT_01" ,"LY_RAP_0003_Dia_FoT_03")
-ffpe$barcode =c("15:S12966E", "15:S12966A", "15:S12966C")
-ffpe$Tissue_Site = c("left_breast", "right_neck_LN", "left_axilla_LN")
-ffpe$Specimen_Type = "FFPE"
-ffpe$DNA = "DNA"
-ffpe$STUDY_PATIENT_ID = "LY_RAP_0003"
-dna = rbind(dna, ffpe)
-dna$id = paste(dna$Specimen_Type, dna$Tissue_Site, dna$barcode, sep="_")
-
 #----------------------------------------------------------------------
 #purpose
 #----------------------------------------------------------------------
 
-#summarized snvs and cnvs from 21 sequencing folders
-#here, summarize number of mutations/sample/location
-#which genes are mutated across all sites which are unique?
+#prepare input files for pyclone
 
 #----------------------------------------------------------------------
 #data
@@ -71,18 +45,16 @@ t=t[order(V1)]
 make_input = function(patient){
   pat_dat = as.data.table(filter(muts, id == patient))
   pat_dat$normal_cn = 2
-  pat_dat$Indiv = NULL
-  pat_dat = merge(pat_dat, dna, by = c("id"))
-
-  pat_dat = unique(pat_dat[,c("mut_id", "Ref_counts", "alt_counts", "normal_cn", "Nmin", "Nmaj", "hg19.ensemblToGeneName.value", "Func.ensGene", "Indiv")])
-  print(table(pat_dat$Nmaj))
+  pat_dat = unique(pat_dat[,c("mut_id", "Ref_counts", "alt_counts", "normal_cn",
+  "MinorCN", "MajorCN", "symbol", "Func.ensGene", "id")])
+  print(table(pat_dat$MajorCN))
 
   z = which(pat_dat$mut_id %in% t$V1)
   pat_dat = pat_dat[z,]
 
   colnames(pat_dat) = c("mutation_id", "ref_counts", "var_counts", "normal_cn", "minor_cn", "major_cn", "gene_name", "region", "id")
   print(tail(pat_dat))
-  write.table(pat_dat, file=paste("/cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/Pyclone/", pat_dat$id[1], "pyclone_input.tsv", sep="_"), quote=F, row.names=F, sep="\t")
+  write.table(pat_dat, file=paste(pat_dat$id[1], "pyclone_input.tsv", sep="_"), quote=F, row.names=F, sep="\t")
 }
 
 llply(pats, make_input)
