55384136973c43dd493d283fe652ad6fe91ee5de
karini925 <karin.isaev@gmail.com>
2020-07-29 12:23:31 -0400

    citup update

--- Part_4_Phylogeny_analysis/Pyclone_CITUP/pyclone_004_setting_up_CITUP.sh
+++ Part_4_Phylogeny_analysis/Pyclone_CITUP/pyclone_004_setting_up_CITUP.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -p veryhimem
-#SBATCH --mem=184320M
+#SBATCH -p himem
+#SBATCH --mem=61440M
 #SBATCH -J citup
 #SBATCH -c 8
 #SBATCH -t 5-00:00 # Runtime in D-HH:MM
@@ -11,11 +11,11 @@ cd /cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/Pyclone
 module load citup
 module load python3
 
-#run_citup_qip.py 2020-07-27_rap_citup_input_freqs.txt \
-#2020-07-27_rap_citup_input_clusters.txt p003_results.h5 --submit local
+run_citup_qip.py 2020-07-29_rap_citup_input_freqs.txt \
+2020-07-29_rap_citup_input_clusters.txt p003_results.h5 --submit local
 
 #less 2020-07-27_rap_citup_input_freqs.txt | head -100 > test_citup_input_freqs.txt
 #less 2020-07-27_rap_citup_input_clusters.txt | head -100 > test_citup_input_clusters.txt
 
-run_citup_qip.py test_citup_input_freqs.txt \
-test_citup_input_clusters.txt test.h5 --submit local
+#run_citup_qip.py test_citup_input_freqs.txt \
+t#est_citup_input_clusters.txt test.h5 --submit local
