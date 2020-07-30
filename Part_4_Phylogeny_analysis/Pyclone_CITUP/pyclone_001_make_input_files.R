303292fdaddcda8242746f738a1674a53e9bcb8d
karini925 <karin.isaev@gmail.com>
2020-07-30 09:57:02 -0400

    set up pyclone all mutations

--- Part_4_Phylogeny_analysis/Pyclone_CITUP/pyclone_001_make_input_files.R
+++ Part_4_Phylogeny_analysis/Pyclone_CITUP/pyclone_001_make_input_files.R
@@ -35,26 +35,34 @@ library(plotly)
 
 #1. SNV/CNA input data for pyclone
 
-muts = fread(list.files(pattern="PYCLONE_INPUT_MUTS.txt")[length(list.files(pattern="PYCLONE_INPUT_MUTS.txt"))]) #get most recent mutation file
-pats = unique(muts$id)
-t = as.data.table(table(muts$mut_id))
-t=t[order(V1)]
+make_input_pyclone = function(input_muts, type){
+  muts = fread(input_muts) #get most recent mutation file
+  pats = unique(muts$id)
+  t = as.data.table(table(muts$mut_id))
+  t=t[order(V1)]
 
-#make sure mutations ordered in the same way in each sample specific file
+  #make sure mutations ordered in the same way in each sample specific file
 
-make_input = function(patient){
-  pat_dat = as.data.table(filter(muts, id == patient))
-  pat_dat$normal_cn = 2
-  pat_dat = unique(pat_dat[,c("mut_id", "Ref_counts", "alt_counts", "normal_cn",
-  "MinorCN", "MajorCN", "symbol", "Func.ensGene", "id")])
-  print(table(pat_dat$MajorCN))
+  make_input = function(patient){
+    pat_dat = as.data.table(filter(muts, id == patient))
+    pat_dat$normal_cn = 2
+    pat_dat = unique(pat_dat[,c("mut_id", "Ref_counts", "alt_counts", "normal_cn",
+    "MinorCN", "MajorCN", "symbol", "Func.ensGene", "id")])
+    print(table(pat_dat$MajorCN))
 
-  z = which(pat_dat$mut_id %in% t$V1)
-  pat_dat = pat_dat[z,]
+    z = which(pat_dat$mut_id %in% t$V1)
+    pat_dat = pat_dat[z,]
 
-  colnames(pat_dat) = c("mutation_id", "ref_counts", "var_counts", "normal_cn", "minor_cn", "major_cn", "gene_name", "region", "id")
-  print(tail(pat_dat))
-  write.table(pat_dat, file=paste(pat_dat$id[1], "pyclone_input.tsv", sep="_"), quote=F, row.names=F, sep="\t")
+    colnames(pat_dat) = c("mutation_id", "ref_counts", "var_counts", "normal_cn", "minor_cn", "major_cn", "gene_name", "region", "id")
+    print(tail(pat_dat))
+    write.table(pat_dat, file=paste(pat_dat$id[1], type, "pyclone_input.tsv", sep="_"), quote=F, row.names=F, sep="\t")
+  }
+
+  llply(pats, make_input)
+
 }
 
-llply(pats, make_input)
+#make input for all muts
+make_input_pyclone("2020-07-30_full_mutations_PYCLONE_INPUT_MUTS.txt", "all_muts")
+#make input for some muts
+make_input_pyclone("2020-07-30_subset_mutations_PYCLONE_INPUT_MUTS.txt", "subset_muts")
---
+++ Part_4_Phylogeny_analysis/Pyclone_CITUP/pyclone_002_run_main_program_all_muts.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH -p veryhimem
+#SBATCH --mem=175440M
+#SBATCH -J pyclone_all
+#SBATCH -c 8
+#SBATCH -t 5-00:00 # Runtime in D-HH:MM
+
+cd /cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/Pyclone
+
+module load python2
+module load pyclone/0.13.1
+
+#patient
+PyClone run_analysis_pipeline --tumour_contents 0.789 0.7359 0.6651 0.96266 \
+0.91772 0.7864 \
+0.96735 0.824 0.94581 0.9119 0.8574 0.804 0.96301 \
+0.93695 0.8643 0.95118 0.95338 0.8487 0.95055 0.95822 \
+--in_files FFPE_left_breast_15\:S12966E_all_muts_pyclone_input.tsv \
+FFPE_left_axilla_LN_15:S12966C_all_muts_pyclone_input.tsv \
+FFPE_right_neck_LN_15\:S12966A_all_muts_pyclone_input.tsv \
+FT_Adrenal\ gland\,\ NOS_341502_all_muts_pyclone_input.tsv \
+FT_Stomach\,\ NOS_341480_all_muts_pyclone_input.tsv \
+FT_Bladder\,\ NOS_341468_all_muts_pyclone_input.tsv \
+FT_Spleen_341462_all_muts_pyclone_input.tsv \
+FT_Pancreas\,\ NOS_341373_all_muts_pyclone_input.tsv \
+FT_Parotid\ gland_341369_all_muts_pyclone_input.tsv \
+FT_Kidney\,\ NOS_341364_all_muts_pyclone_input.tsv \
+FT_Kidney\,\ NOS_341360_all_muts_pyclone_input.tsv \
+FT_Mediastinum\,\ NOS_341355_all_muts_pyclone_input.tsv \
+FT_Cecum_341166_all_muts_pyclone_input.tsv \
+FT_Retroperitoneum\,\ NOS_340898_all_muts_pyclone_input.tsv \
+FT_Inguinal\ region\,\ NOS_340893_all_muts_pyclone_input.tsv \
+FT_Abdomen\,\ NOS_340889_all_muts_pyclone_input.tsv \
+FT_Omentum_340885_all_muts_pyclone_input.tsv \
+FT_Shoulder\,\ NOS_340881_all_muts_pyclone_input.tsv \
+FT_Cervical\ lymph\ node_340867_all_muts_pyclone_input.tsv \
+FT_Axilla\,\ NOS_340852_all_muts_pyclone_input.tsv \
+ --working_dir Pyclone_all_muts_July2020
+
+PyClone build_table --config_file Pyclone_all_muts_July2020/config.yaml --out_file Pyclone_all_muts_July2020_table_file.txt --table_type loci
+PyClone build_table --config_file Pyclone_all_muts_July2020/config.yaml --out_file Pyclone_all_muts_July2020_table_file_cluster.txt --table_type cluster
