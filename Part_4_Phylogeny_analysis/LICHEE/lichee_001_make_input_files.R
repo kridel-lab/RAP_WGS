425e7f1cb7e8a223e0d77417e28284de38f47506
karini925 <karin.isaev@gmail.com>
2020-09-13 11:26:55 -0400

    pyclone

---
+++ Part_4_Phylogeny_analysis/LICHEE/lichee_001_make_input_files.R
@@ -0,0 +1,83 @@
+#----------------------------------------------------------------------
+#----------------------------------------------------------------------
+
+#----------------------------------------------------------------------
+#load functions and libraries
+#----------------------------------------------------------------------
+
+date = Sys.Date()
+print(date)
+
+options(stringsAsFactors=F)
+setwd("/cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/Pyclone")
+
+#load libraries
+packages <- c("dplyr", "readr", "ggplot2", "vcfR", "tidyr", "mclust", "data.table",
+              "plyr", "openxlsx",
+              "ggrepel", "stringr", "maftools", "magrittr", "ggExtra", "broom", "ggthemes")
+lapply(packages, require, character.only = TRUE)
+
+#----------------------------------------------------------------------
+#purpose
+#----------------------------------------------------------------------
+
+#prepare input files for pyclone-vi
+#Input format
+#mutation_id - Unique identifier for the mutation. This is free form but should match across all samples.
+#sample_id - Unique identifier for the sample.
+#ref_counts - Number of reads matching the reference allele.
+#alt_counts - Number of reads matching the alternate allele.
+#major_cn - Major copy number of segment overlapping mutation.
+#minor_cn - Minor copy number of segment overlapping mutation.
+#normal_cn - Total copy number of segment in healthy tissue. For autosome this will be two and male sex chromosomes one.
+#tumour_content - The tumour content (cellularity) of the sample. Default value is 1.0 if column is not present
+
+#----------------------------------------------------------------------
+#data
+#----------------------------------------------------------------------
+
+#1. purity and sample data
+purity=fread("/cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/Palimpsest/input/annotation_data_palimpsest_input.txt")
+colnames(purity)[1] = "Indiv"
+purity$Gender = NULL
+
+samps = fread("/cluster/projects/kridelgroup/RAP_ANALYSIS/data/RAP_samples_information.txt")
+samps = samps %>% select(id, Indiv)
+samps = merge(samps, purity, by = "Indiv")
+
+#2. SNV/CNA input data for pyclone
+
+make_input_pyclone = function(input_muts, type){
+  muts = fread(input_muts) #get most recent mutation file
+  pats = unique(muts$id)
+  t = as.data.table(table(muts$mut_id))
+  t=t[order(V1)]
+
+  #make sure mutations ordered in the same way in each sample specific file
+  #try analysis assuming mutations are copy neutral
+  #muts$MajorCN=1
+  #muts$MinorCN=1
+  #muts$tot_cn = muts$MajorCN + muts$MinorCN
+  #muts_keep = as.data.table(filter(muts, tot_cn <= 4))
+  muts_sum = filter(as.data.table(table(muts$mut_id)), N ==20)$V1
+  muts = as.data.table(filter(muts, mut_id %in% muts_sum))
+  muts = merge(muts, samps, by = "id")
+
+  #prepare matrix for pyclone vi input
+  muts$normal_cn = 2
+  muts = as.data.table(filter(muts, mut_id %in% muts_sum))
+
+  muts = muts %>% select(mut_id, id, Ref_counts, alt_counts, MajorCN, MinorCN, normal_cn, Purity)
+  colnames(muts) = c("mutation_id", "sample_id", "ref_counts", "alt_counts", "major_cn", "minor_cn", "normal_cn", "tumour_content")
+  print(table(muts$major_cn))
+
+  print(tail(muts))
+  print(dim(muts))
+  write.table(muts, file=paste("all_samples_pyclonevi", type, "pyclone_input.tsv", sep="_"), quote=F, row.names=F, sep="\t")
+
+}
+
+#make input for all muts
+make_input_pyclone("2020-09-12_full_mutations_PYCLONE_INPUT_MUTS.txt", "all_muts")
+#make input for some muts
+make_input_pyclone("2020-09-12_subset_mutations_PYCLONE_INPUT_MUTS.txt", "subset_muts")
--- Part_4_Phylogeny_analysis/LICHEE/run_LICHEE.sh
+++ Part_4_Phylogeny_analysis/LICHEE/run_LICHEE.sh
@@ -1,5 +1,5 @@
 #this tool: 
-#was run locally in order to simplify visualization 
+#was run locally in order to simplify visualization
 #Karin Isaev
 #August 23rd, 2019
 
@@ -7,14 +7,14 @@
 
 cd /Users/kisaev/lichee/LICHeE/release
 
-lichee_input=/Users/kisaev/Documents/RAP_ANALYSIS/2019-08-23_liche_input_somatic_muts.txt	
+lichee_input=/Users/kisaev/Documents/RAP_ANALYSIS/2019-08-23_liche_input_somatic_muts.txt
 less $lichee_input | head -6000 > test_lichee.txt
 
-#run 
-./lichee -build -i $lichee_input -n 0 -showTree 1 -color -dot -sampleProfile 
+#run
+./lichee -build -i $lichee_input -n 0 -showTree 1 -color -dot -sampleProfile
 #test
-./lichee -build -i test_lichee.txt -n 0 -showTree 1 -color -dot -sampleProfile 
+./lichee -build -i test_lichee.txt -n 0 -showTree 1 -color -dot -sampleProfile
 
-#on the cluster 
-lichee_input=/cluster/projects/kridelgroup/RAP_ANALYSIS/chr/vcfs_final/2019-08-23_liche_input_somatic_muts.txt	
-./lichee -build -i $lichee_input -n 0 -showTree 1 -color -dot -sampleProfile 
+#on the cluster
+lichee_input=/cluster/projects/kridelgroup/RAP_ANALYSIS/chr/vcfs_final/2019-08-23_liche_input_somatic_muts.txt
+./lichee -build -i $lichee_input -n 0 -showTree 1 -color -dot -sampleProfile
