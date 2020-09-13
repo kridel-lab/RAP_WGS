b5f1dcdc91e02e6e184078f83abab95ee33af54f
karini925 <karin.isaev@gmail.com>
2020-09-13 18:54:10 -0400

    update pyclone

--- Part_4_Phylogeny_analysis/Pyclone_CITUP/pyclone-vi_002_run_main_program_all_muts.sh
+++ Part_4_Phylogeny_analysis/Pyclone_CITUP/pyclone-vi_002_run_main_program_all_muts.sh
@@ -10,5 +10,5 @@ cd /cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/Pyclone
 source activate pyclone-vi
 module load python3
 
-pyclone-vi fit -i all_samples_pyclonevi_all_muts_pyclone_input.tsv -o rap_wgs_all_muts.h5 -c 40 -d beta-binomial -r 10
+pyclone-vi fit -i all_samples_pyclonevi_all_muts_pyclone_input.tsv -o rap_wgs_all_muts.h5 -c 40 -d binomial -r 50
 pyclone-vi write-results-file -i rap_wgs_all_muts.h5 -o rap_wgs_all_muts.tsv
