684a1f320906603d6ddaa16d4fee40039bb11c69
karini925 <karin.isaev@gmail.com>
2020-11-05 11:14:30 -0500

    run get pipeup summaries on tumour samples

--- Part_1_Somatic_variant_calling/Mutect2/pipeline_002A_running_GetPileupSummaries.sh
+++ Part_1_Somatic_variant_calling/Mutect2/pipeline_002A_running_GetPileupSummaries.sh
@@ -2,14 +2,15 @@
 #
 #SBATCH -N 1 # Ensure that all cores are on one machine
 #SBATCH -p himem
-#SBATCH --mem=61440M
+#SBATCH --mem=41440M
 #SBATCH -t 5-00:00 # Runtime in D-HH:MM
 #SBATCH -J MUTECT2
-#SBATCH --array=0-479 # job array index
+#SBATCH --array=0-647 # job array index
 
 #need to run Mutect2 on all bam files from tumour samples
 #author: Karin Isaev
 #date started: June 25, 2019
+#date updated: November 5th, 2020
 
 module load java/8  #8
 module load samtools
@@ -18,21 +19,11 @@ module load gatk
 module load annovar
 
 #pwd
-#/cluster/projects/kridelgroup/RAP_ANALYSIS/chr
+cd /cluster/projects/burst2
+#ls */*.bam > all_chrs_bams.txt
 
-#this is part 2 of the best protocols GATK steps for identifying SNVs and INDELS
-#https://software.broadinstitute.org/gatk/best-practices/workflow?id=11146
-
-#Summarizes counts of reads that support reference, alternate and 
-#other alleles for given sites. Results can be used with CalculateContamination.
-
-#using chromosome specific bam files
-
-# get file name
-#find -L . -name "*.cram.bai" > mutect_jobs #480
-#sed 's/.bai//' mutect_jobs > mutect_jobs_clean
-
-names=($(cat mutect_jobs_clean))
+#pwd
+names=($(cat all_chrs_bams.txt))
 echo ${names[${SLURM_ARRAY_TASK_ID}]}
 
 tum=($(samtools view -H ${names[${SLURM_ARRAY_TASK_ID}]} | grep '^@RG' | sed "s/.*SM:\([^\t]*\).*/\1/g" | uniq))
@@ -42,11 +33,15 @@ export tum
 chr=($(echo ${names[${SLURM_ARRAY_TASK_ID}]} | awk -F'[_.]' '{print $8}'))
 echo "${chr}"
 
+patient_name=${tum%_*_*_*}
+normal_file=/cluster/projects/kridelgroup/RAP_ANALYSIS/MUTECT2_WORKDIR/CHR_split/${patient_name}_${chr}.bam
+
+fasta=/cluster/projects/kridelgroup/RAP_ANALYSIS/human_g1k_v37_decoy.fasta #from gatk resource bundle
+output=/cluster/projects/burst2/MUTECT2_pileup
+
 gatk GetPileupSummaries \
--R /cluster/projects/kridelgroup/RAP_ANALYSIS/human_g1k_v37_decoy.fasta \
+-R $fasta \
 -I "${names[${SLURM_ARRAY_TASK_ID}]}" \
 -L "${chr}" \
--O ${names[${SLURM_ARRAY_TASK_ID}]}.pileups.table \
+-O ${output}/${tum}_${chr}.pileups.table \
 -V /cluster/projects/kridelgroup/RAP_ANALYSIS/af-only-gnomad.raw.sites.b37.vcf.gz
-
-
