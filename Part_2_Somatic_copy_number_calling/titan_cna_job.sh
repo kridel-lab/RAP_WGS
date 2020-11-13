61fc3e70e2ba2b5365a4a303b4cd1f926e100fb6
karini925 <karin.isaev@gmail.com>
2020-11-13 14:04:07 -0500

    run second part of titancna

--- Part_2_Somatic_copy_number_calling/titan_cna_job.sh
+++ Part_2_Somatic_copy_number_calling/titan_cna_job.sh
@@ -13,14 +13,14 @@ module load R/3.6.1
 ###-------PART 1---------------------------------------------------------------------------------------
 cluster=/cluster/home/kisaev/TitanCNA/scripts/snakemake/config/cluster_slurm.yaml
 icho=/cluster/home/kisaev/TitanCNA/scripts/snakemake/ichorCNA.snakefile
-snakemake -s $icho --cluster-config $cluster --cluster "sbatch -p himem --mem=51440M -t 5-00:00" -j 50
+#snakemake -s $icho --cluster-config $cluster --cluster "sbatch -p himem --mem=51440M -t 5-00:00" -j 50
 
 #done! after finished running, comment out "icho" and snakemake command above and run the icho and
 #snakemake below then repeat for part 3
 
 ###-------PART 2---------------------------------------------------------------------------------------
-#icho=/cluster/home/kisaev/TitanCNA/scripts/snakemake/getAlleleCounts.snakefile
-#snakemake -s $icho --cluster-config $cluster --cluster "sbatch -p himem --mem=51440M -t 5-00:00" -j 50
+icho=/cluster/home/kisaev/TitanCNA/scripts/snakemake/getAlleleCounts.snakefile
+snakemake -s $icho --cluster-config $cluster --cluster "sbatch -p himem --mem=51440M -t 5-00:00" -j 50
 
 ###-------PART 3---------------------------------------------------------------------------------------
 #icho=/cluster/home/kisaev/TitanCNA/scripts/snakemake/TitanCNA.snakefile
