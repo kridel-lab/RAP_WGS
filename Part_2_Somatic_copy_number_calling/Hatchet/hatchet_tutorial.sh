0d76764059e3a668f3be8ab689e3c3ad789c428a
karini925 <karin.isaev@gmail.com>
2021-01-12 09:34:16 -0500

    run rest of pipeline

--- Part_2_Somatic_copy_number_calling/Hatchet/hatchet_tutorial.sh
+++ Part_2_Somatic_copy_number_calling/Hatchet/hatchet_tutorial.sh
@@ -94,4 +94,23 @@ cd ${XDIR}
 #
 
 #BNPY=/cluster/home/kisaev/bnpy/bnpy/
-\time -v python2 -m hatchet cluBB ${BB}bulk.bb -o ${BBC}bulk.seg -O ${BBC}bulk.bbc -e ${RANDOM} -tB 0.04 -tR 0.15 -d 0.08
+#\time -v python2 -m hatchet cluBB ${BB}bulk.bb -o ${BBC}bulk.seg -O ${BBC}bulk.bbc -e ${RANDOM} -tB 0.04 -tR 0.15 -d 0.08
+
+cd ${ANA}
+\time -v python2 -m hatchet BBot -c RD --figsize 6,3 ${BBC}bulk.bbc &
+\time -v python2 -m hatchet BBot -c CRD --figsize 6,3 ${BBC}bulk.bbc &
+\time -v python2 -m hatchet BBot  -c BAF --figsize 6,3 ${BBC}bulk.bbc &
+\time -v python2 -m hatchet BBot  -c BB ${BBC}bulk.bbc &
+\time -v python2 -m hatchet BBot  -c CBB ${BBC}bulk.bbc -tS 0.01 &
+wait
+
+cd ${RES}
+\time -v python2 -m hatchet solve -i ${BBC}bulk -n2,8 -p 400 -v 3 -u 0.03 -r ${RANDOM} -j ${J} -eD 6 -eT 12 -g 0.35 -l 0.6 &> >(tee >(grep -v Progress > hatchet.log))
+
+## Increase -l to 0.6 to decrease the sensitivity in high-variance or noisy samples, and decrease it to -l 0.3 in low-variance samples to increase the sensitivity and explore multiple solutions with more clones.
+## Increase -u if solutions have clone proportions equal to the minimum threshold -u
+## Decrease the number of restarts to 200 or 100 for fast runs, as well as user can decrease the number of clones to -n 2,6 when appropriate or when previous runs suggest fewer clones.
+## Increase the single-clone confidence to `-c 0.6` to increase the confidence in the presence of a single tumor clone and further increase this value when interested in a single clone.
+
+cd ${EVA}
+\time -v python2 -m hatchet BBeval ${RES}/best.bbc.ucn
