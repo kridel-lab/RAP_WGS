ffd1f510a6bd8144b45ac734e28ebee73db70e5a
karini925 <karin.isaev@gmail.com>
2021-01-18 11:31:36 -0500

    prep snv data for hatchet integration

--- Part_1_Somatic_variant_calling/Merged_Variant_Callers/Merged_007_summary_driver_genes_across_samples.R
+++ Part_1_Somatic_variant_calling/Merged_Variant_Callers/Merged_007_summary_driver_genes_across_samples.R
@@ -1,23 +1,11 @@
-#----------------------------------------------------------------------
-#karin isaev
-#----------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#Merged_007_summary_driver_genes_across_samples.R
+#Karin Isaev
+#Monday January 18th, 2020
+#-------------------------------------------------------------------------------
 
-date = Sys.Date()
-print(date)
-options(scipen=999) #no scientific notation
-
-#----------------------------------------------------------------------
-#load functions and libraries
-#----------------------------------------------------------------------
-
-options(stringsAsFactors=F)
-
-#load libraries
-packages <- c("dplyr", "ggplot2", "tidyr", "data.table", "plyr",
-	"stringr")
-lapply(packages, require, character.only = TRUE)
-library("readxl")
-library(GenomicRanges)
+#load packages and data
+source("/cluster/home/kisaev/RAP_WGS/config-file.R")
 
 #----------------------------------------------------------------------
 #purpose
@@ -27,89 +15,8 @@ library(GenomicRanges)
 #check which mutations occur in all samples versus only 1 or several
 
 #----------------------------------------------------------------------
-#data
+#Analysis
 #----------------------------------------------------------------------
-
-setwd("/cluster/projects/kridelgroup/RAP_ANALYSIS/merged_MUTECT2_STRELKA/merged_variants_vcfs/vcf_summary_text")
-
-#DLBCL driver genes from Reddy et al 2017
-reddy = as.data.table(read_excel("/cluster/projects/kridelgroup/RAP_ANALYSIS/data/Reddyetal_2017_driver_mutations.xlsx"))
-reddy$type = "DLBCL"
-
-#PMBCL genes
-pmbcl = as.data.table(read_excel("/cluster/projects/kridelgroup/RAP_ANALYSIS/data/PMBCL_genes.xlsx"))
-pmbcl$type = "PMBCL"
-pmbcl = unique(pmbcl)
-
-#MCL genes
-mcl = as.data.table(read_excel("/cluster/projects/kridelgroup/RAP_ANALYSIS/data/MCL_genes.xlsx"))
-mcl$type = "MCL"
-mcl = unique(mcl)
-
-all_drivers = rbind(reddy, pmbcl, mcl)
-
-#DLBCL mutations from Morin Blood 2013
-morin = as.data.table(read_excel("/cluster/projects/kridelgroup/RAP_ANALYSIS/data/supp_blood-2013-02-483727_TableS3.xlsx"))
-genes_sum=as.data.table(table(morin$Gene))
-genes_sum = as.data.table(filter(genes_sum, N > 5))
-colnames(genes_sum)=c("Gene", "num_samples_w_mut")
-
-#Our mutations
-read_only_snvs = readRDS(list.files(pattern="READ_ONLY_ALL_MERGED_MUTS.rds")[length(list.files(pattern="READ_ONLY_ALL_MERGED_MUTS.rds"))])
-read_only_indels = readRDS(list.files(pattern="READ_ONLY_ALL_MERGED_MUTS_INDELS.rds")[length(list.files(pattern="READ_ONLY_ALL_MERGED_MUTS_INDELS.rds"))])
-read_only_snvs$mut_type = "SNV"
-read_only_indels$mut_type = "INDEL"
-
-read_only = rbind(read_only_snvs, read_only_indels)
-
-#sample info
-samps = readRDS("/cluster/projects/kridelgroup/RAP_ANALYSIS/copy_RAP_masterlist_samples.rds")
-colnames(samps)[4] ="Indiv"
-z = which(samps$Indiv %in% read_only$Indiv)
-samps = samps[z,]
-samps[18,2] = "Kidney, NOS 2"
-
-read_only$Tissue_Site = NULL
-read_only$STUDY_PATIENT_ID = NULL
-
-read_only = merge(read_only, samps, by = "Indiv")
-
-#----------------------------------------------------------------------
-#analysis
-#----------------------------------------------------------------------
-
-#mut-gene summary table for downstream use
-mut_gene = unique(read_only[,c("mut_id", "symbol", "biotype", "Func.ensGene", "ExonicFunc.ensGene",
-"AAChange.ensGene", "cosmic68", "mut_type")])
-
-#1. how many patient samples is each mutation found in?
-samples_per_mut = as.data.table(table(read_only$mut_id, read_only$STUDY_PATIENT_ID))
-
-colnames(samples_per_mut) = c("mut_id", "patient", "num_of_samples_with_mut")
-samples_per_mut = merge(samples_per_mut, mut_gene, by="mut_id")
-samples_per_mut = samples_per_mut[order(-num_of_samples_with_mut)]
-samples_per_mut = as.data.table(filter(samples_per_mut, num_of_samples_with_mut >0))
-
-samples_per_mut$phylogeny = ""
-z = which(samples_per_mut$patient == "LY_RAP_0001")
-LY_RAP_0001 = samples_per_mut[z,]
-LY_RAP_0001$phylogeny[LY_RAP_0001$num_of_samples_with_mut == 3] = "ancestor"
-LY_RAP_0001$phylogeny[LY_RAP_0001$num_of_samples_with_mut == 1] = "private"
-LY_RAP_0001$phylogeny[LY_RAP_0001$phylogeny == ""] = "shared"
-
-z = which(samples_per_mut$patient == "LY_RAP_0002")
-LY_RAP_0002 = samples_per_mut[z,]
-LY_RAP_0002$phylogeny[LY_RAP_0002$num_of_samples_with_mut == 4] = "ancestor"
-LY_RAP_0002$phylogeny[LY_RAP_0002$num_of_samples_with_mut == 1] = "private"
-LY_RAP_0002$phylogeny[LY_RAP_0002$phylogeny == ""] = "shared"
-
-z = which(samples_per_mut$patient == "LY_RAP_0003")
-LY_RAP_0003 = samples_per_mut[z,]
-LY_RAP_0003$phylogeny[LY_RAP_0003$num_of_samples_with_mut == 20] = "ancestor"
-LY_RAP_0003$phylogeny[LY_RAP_0003$num_of_samples_with_mut == 1] = "private"
-LY_RAP_0003$phylogeny[LY_RAP_0003$phylogeny == ""] = "shared"
-
-samples_per_mut = rbind(LY_RAP_0001, LY_RAP_0002, LY_RAP_0003)
 
 #data table for barplot
 barplot = as.data.table(table(samples_per_mut$num_of_samples_with_mut, samples_per_mut$patient))
--- Part_2_Somatic_copy_number_calling/Hatchet/prep_SNVs_for_CCF_calculation.R
+++ Part_2_Somatic_copy_number_calling/Hatchet/prep_SNVs_for_CCF_calculation.R
@@ -6,3 +6,53 @@
 
 #load packages and data
 source("/cluster/home/kisaev/RAP_WGS/config-file.R")
+
+#-------------------------------------------------------------------------------
+#Purpose
+#-------------------------------------------------------------------------------
+
+#prepare SNV data in format so that it can be run through Hatchet's script
+#to extract CCF for every mutation using copy number calls form Hatchet
+#ccf=cancer cell fraction
+#these CCFs can then be converted into VAF via CCF/2
+#these VAF values can then be run through Sciclone to get clusters
+#and then through cloneevol to get a tree
+
+#-------------------------------------------------------------------------------
+#Analysis
+#-------------------------------------------------------------------------------
+
+get_muts = function(patient){
+    #subset mutation data for given patient
+    #format required
+    #CSV file with the following fields
+    #(whose names must be specified in the first-row header)
+    #chrom, position, Patient, Sample, somatic_status (Somatic),
+    #tumor_var_freq, tumor_reads1 (REF count), tumor_reads2 (ALT count)
+
+    #start
+    print(patient)
+
+    #get data
+    pat_dat = as.data.table(filter(read_only, STUDY_PATIENT_ID == patient))
+    pat_dat = pat_dat[,c("CHROM", "POS", "STUDY_PATIENT_ID", "Sample", "biotype",
+      "gt_AF", "Ref_counts", "alt_counts")]
+    colnames(pat_dat)[which(colnames(pat_dat)=="biotype")] = "somatic_status"
+    pat_dat$somatic_status = "Somatic"
+    #remove "chr"
+    pat_dat$CHROM = sapply(pat_dat$CHROM, function(x){unlist(strsplit(x, "chr"))[2]})
+    colnames(pat_dat)=c("chrom", "position", "Patient", "Sample", "somatic_status",
+    "tumor_var_freq", "tumor_reads1", "tumor_reads2")
+
+    #prep output file
+    output_file=paste("/cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/Hatchet/", patient,".csv", sep="")
+
+    #write file
+    write.csv(pat_dat, output_file, quote=F)
+    print("done")
+}
+
+patients = c("LY_RAP_0001", "LY_RAP_0002", "LY_RAP_0003")
+llply(patients, get_muts)
+
+print("done all samples prep")
