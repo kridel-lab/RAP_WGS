78de88a6109da9d94d8ca2ed656459b3536b3d88
karini925 <karin.isaev@gmail.com>
2021-02-03 15:46:37 -0500

    run collect bamreadcounts

--- Part_1_Somatic_variant_calling/Merged_Variant_Callers/Merged_008_get_pyclone_missing_counts.sh
+++ Part_1_Somatic_variant_calling/Merged_Variant_Callers/Merged_008_get_pyclone_missing_counts.sh
@@ -5,8 +5,14 @@
 #SBATCH --mem=31440M
 #SBATCH -t 5-00:00 # Runtime in D-HH:MM
 #SBATCH -J get_missing_counts
+#SBATCH --array=0-2 # job array index
+
+cd /cluster/projects/kridelgroup/RAP_ANALYSIS
+
+names=($(cat patients_three_names.txt))
+echo ${names[${SLURM_ARRAY_TASK_ID}]}
+pat=${names[${SLURM_ARRAY_TASK_ID}]}
+echo $pat
 
 script=/cluster/home/kisaev/RAP_WGS/Part_1_Somatic_variant_calling/Merged_Variant_Callers/Merged_008_get_pyclone_missing_counts.R
-Rscript $script LY_RAP_0001
-Rscript $script LY_RAP_0002
-Rscript $script LY_RAP_0003
+Rscript $script $pat
