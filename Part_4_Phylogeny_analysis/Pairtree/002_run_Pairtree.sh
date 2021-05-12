5e5ccac25e5d928073d92ed351356be3d516bf81
karini925 <karin.isaev@gmail.com>
2021-05-12 16:08:07 -0400

    fix bash script

--- Part_4_Phylogeny_analysis/Pairtree/002_run_Pairtree.sh
+++ Part_4_Phylogeny_analysis/Pairtree/002_run_Pairtree.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -N 1 # Ensure that all cores are on one machine
-#SBATCH -p himem
-#SBATCH --mem=61440M
-#SBATCH -t 5-00:00 # Runtime in D-HH:MM
-#SBATCH -J run pairtree
+#SBATCH -p long
+#SBATCH --mem=30720M
+#SBATCH -J pairtree
+#SBATCH -c 10
+#SBATCH -t 21-00:00 # Runtime in D-HH:MM
 
 conda deactivate
 conda activate pairtree
