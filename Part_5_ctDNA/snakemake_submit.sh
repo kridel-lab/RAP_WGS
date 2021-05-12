4b1710a87bbd680c2f4344c3d1d9157128b36022
karini925 <karin.isaev@gmail.com>
2021-05-12 11:58:23 -0400

    update latency wait time

--- Part_5_ctDNA/snakemake_submit.sh
+++ Part_5_ctDNA/snakemake_submit.sh
@@ -27,4 +27,4 @@ export TMP=/cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/ConsensusCruncher
 #then manually renamed files using this conversion provided by Michael Hong
 #KLCS_0084=LY_RAP_0001, KLCS_0085=LY_RAP_0002, KLCS_0086=LY_RAP_0003
 
-snakemake -s /cluster/home/kisaev/RAP_WGS/Part_5_ctDNA/Consensus_Cruncher_Pipeline.snakefile -j56 --latency-wait 86400 --cluster-config /cluster/home/kisaev/RAP_WGS/Part_5_ctDNA/config/cluster.yaml --cluster-sync "sbatch -p {cluster.partition} --mem={cluster.mem} -t {cluster.time} -c {cluster.ncpus} -o {cluster.out}" --rerun-incomplete --nolock
+snakemake -s /cluster/home/kisaev/RAP_WGS/Part_5_ctDNA/Consensus_Cruncher_Pipeline.snakefile -j56 --latency-wait 432000 --cluster-config /cluster/home/kisaev/RAP_WGS/Part_5_ctDNA/config/cluster.yaml --cluster-sync "sbatch -p {cluster.partition} --mem={cluster.mem} -t {cluster.time} -c {cluster.ncpus} -o {cluster.out}" --rerun-incomplete --nolock
