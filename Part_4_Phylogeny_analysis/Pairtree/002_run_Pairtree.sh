02856e4a2a3d7587d18298390eeedb5761f85fef
karini925 <karin.isaev@gmail.com>
2021-05-12 16:19:38 -0400

    fix conda

--- Part_4_Phylogeny_analysis/Pairtree/002_run_Pairtree.sh
+++ Part_4_Phylogeny_analysis/Pairtree/002_run_Pairtree.sh
@@ -6,8 +6,12 @@
 #SBATCH -t 21-00:00 # Runtime in D-HH:MM
 
 echo ". /cluster/tools/software/python/3.6.5/etc/profile.d/conda.sh" >> ~/.bashrc
-conda deactivate
-conda activate pairtree
+
+#conda deactivate
+#conda activate pairtree
+
+source /cluster/home/kisaev/.bashrc
+source activate pairtree
 
 #set up input files
 input_files=/cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/Pairtree/2021-05-12_input_files
