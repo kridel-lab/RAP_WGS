df551b4abb2a2a354f94c7aec38e80ccd9c41726
karini925 <karin.isaev@gmail.com>
2021-06-16 17:07:15 -0400

    up

--- Part_4_Phylogeny_analysis/Pyclone_CITUP/pyclone_007_mapscape.R
+++ Part_4_Phylogeny_analysis/Pyclone_CITUP/pyclone_007_mapscape.R
@@ -37,9 +37,11 @@ patients = c("LY_RAP_0001", "LY_RAP_0002", "LY_RAP_0003")
 #data
 #----------------------------------------------------------------------
 
-get_mapscape = function(patient){
+get_mapscape_patient_1 = function(patient){
 
   setwd(patient)
+
+  colors = read.xlsx("clones_colours.xlsx")
 
   #adjaceny matrix (made manually for now) harder to extract from clonevol
   citup_ad = read.xlsx("RAP_001_adjacency_matrix.xlsx")
@@ -50,16 +52,116 @@ get_mapscape = function(patient){
   #sample information
   z = which(sample_data$Indiv %in% clone$sample_id)
   samps = sample_data[z,c("Indiv", "Confirmed_by_Robert")]
-  colnames(samps) = c("sample_id", " location_id")
-  samps$x = c(745, 685, 540)
-  samps$y = c(495, 510, 310)
+  colnames(samps) = c("sample_id", "location_id")
+  samps = as.data.frame(samps)
+
+  clone = merge(clone, samps, by = "sample_id")
+  clone = as.data.frame(clone)
+  clone$sample_id = clone$location_id
+  clone$location_id = NULL
+
+  samps$x = c(448, 483, 443)
+  samps$y = c(832, 437, 490)
+  samps = as.data.frame(samps)
+  samps$sample_id = samps$location_id
+  colnames(samps) = c("sample_id", "location_id", "x", "y")
 
   #image of body and tumours
   img_ref = "mapscape_001.png"
 
   #run tool
-  mapscape(clone, citup_ad, samps, img_ref)
+  m = mapscape(clone, citup_ad, samps, img_ref, n_cells =300, clone_colours = colors)
+  return(m)
 
+  print("done yay")
+  setwd(dir_main)
+}
+
+get_mapscape_patient_2 = function(patient){
+
+  setwd(patient)
+
+  colors = read.xlsx("clones_colours.xlsx")
+
+  #adjaceny matrix (made manually for now) harder to extract from clonevol
+  citup_ad = read.xlsx("RAP_002_adjacency_matrix.xlsx")
+
+  #clone and sample data
+  clone = fread("mapscape_input_from_pairtree_pyclonevi.txt")
+
+  #sample information
+  z = which(sample_data$Indiv %in% clone$sample_id)
+  samps = sample_data[z,c("Indiv", "Confirmed_by_Robert")]
+  colnames(samps) = c("sample_id", "location_id")
+  samps = as.data.frame(samps)
+  samps$location_id[samps$location_id == "Adrenal, right"] = "Adrenal"
+
+  clone = merge(clone, samps, by = "sample_id")
+  clone = as.data.frame(clone)
+  clone$sample_id = clone$location_id
+  clone$location_id = NULL
+
+  samps$x = c(449, 522, 373, 452)
+  samps$y = c(835, 445, 806, 733)
+  samps = as.data.frame(samps)
+  samps$sample_id = samps$location_id
+  colnames(samps) = c("sample_id", "location_id", "x", "y")
+
+  #image of body and tumours
+  img_ref = "mapscape_002.png"
+
+  #run tool
+  m = mapscape(clone, citup_ad, samps, img_ref, n_cells =300, clone_colours = colors)
+  return(m)
+  print("done yay")
   setwd(dir_main)
 
 }
+
+get_mapscape_patient_3 = function(patient){
+
+  setwd(patient)
+
+  colors = read.xlsx("clones_colours.xlsx")
+
+  #adjaceny matrix (made manually for now) harder to extract from clonevol
+  citup_ad = read.xlsx("RAP_003_adjacency_matrix.xlsx")
+
+  #clone and sample data
+  clone = fread("mapscape_input_from_pairtree_pyclonevi.txt")
+
+  #sample information
+  z = which(sample_data$Indiv %in% clone$sample_id)
+  samps = sample_data[z,c("Indiv", "New_Tissue_Site")]
+  colnames(samps) = c("sample_id", "location_id")
+  samps = as.data.frame(samps)
+
+  clone = merge(clone, samps, by = "sample_id")
+  clone = as.data.frame(clone)
+  clone$sample_id = clone$location_id
+  clone$location_id = NULL
+
+  samps$x = c(645, 371, 558, 470, 410, 549, 454, 361, 456, 557, 556, 380, 431, 547, 459, 451, 533, 550, 389, 620)
+  samps$y = c(455, 318, 386, 877, 879, 956, 839, 900, 487, 827, 860, 231, 753, 720, 949, 691, 793, 540, 296, 488)
+  samps = as.data.frame(samps)
+  samps$sample_id = samps$location_id
+  colnames(samps) = c("sample_id", "location_id", "x", "y")
+
+  #image of body and tumours
+  img_ref = "mapscape_003.png"
+
+  #run tool
+  m = mapscape(clone, citup_ad, samps, img_ref, n_cells = 100, clone_colours = colors)
+  return(m)
+  print("done yay")
+  setwd(dir_main)
+
+}
+
+get_mapscape_patient_1(patients[1])
+setwd(dir_main)
+
+get_mapscape_patient_2(patients[2])
+setwd(dir_main)
+
+get_mapscape_patient_3(patients[3])
