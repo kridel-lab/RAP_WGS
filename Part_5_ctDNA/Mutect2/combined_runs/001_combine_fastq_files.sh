2953f0e5f8053d3acbb446d867d9b1ba471d37b1
karini925 <karin.isaev@gmail.com>
2021-07-05 14:57:11 -0400

    combine fastq files from two different runs

--- Part_5_ctDNA/Mutect2/combined_runs/001_combine_fastq_files.sh
+++ Part_5_ctDNA/Mutect2/combined_runs/001_combine_fastq_files.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+#
+#SBATCH -N 1 # Ensure that all cores are on one machine
+#SBATCH -J combinefastq
+
 cd /cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/ConsensusCruncher
 
 #old files (May 2021)
@@ -5,3 +10,15 @@ ls /cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/ConsensusCruncher/test_fi
 
 #newer files (July 2021)
 ls /cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/ConsensusCruncher/June30OICRupload
+
+#mkdir RAP_ctDNA_combined_two_runs/fastq_files
+output_dir=/cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/ConsensusCruncher/RAP_ctDNA_combined_two_runs/fastq_files
+
+cat test_files/LY_0001_R1.fastq.gz June30OICRupload/LY_0001_R1.fastq.gz > $output_dir/LY_0001_R1.fastq.gz
+cat test_files/LY_0001_R2.fastq.gz June30OICRupload/LY_0001_R2.fastq.gz > $output_dir/LY_0001_R2.fastq.gz
+
+cat test_files/LY_0002_R1.fastq.gz June30OICRupload/LY_0002_R1.fastq.gz > $output_dir/LY_0002_R1.fastq.gz
+cat test_files/LY_0002_R2.fastq.gz June30OICRupload/LY_0002_R2.fastq.gz > $output_dir/LY_0002_R2.fastq.gz
+
+cat test_files/LY_0003_R1.fastq.gz June30OICRupload/LY_0003_R1.fastq.gz > $output_dir/LY_0003_R1.fastq.gz
+cat test_files/LY_0003_R2.fastq.gz June30OICRupload/LY_0003_R2.fastq.gz > $output_dir/LY_0003_R2.fastq.gz
