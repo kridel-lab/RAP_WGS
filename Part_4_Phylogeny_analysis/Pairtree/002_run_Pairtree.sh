f6fe99b82d0566c49afec24136db50c654e9ce61
karina <karin.isaev@gmail.com>
2021-09-26 19:51:04 -0400

    run pairtree only on sample 1

--- Part_4_Phylogeny_analysis/Pairtree/002_run_Pairtree.sh
+++ Part_4_Phylogeny_analysis/Pairtree/002_run_Pairtree.sh
@@ -14,7 +14,7 @@ source /cluster/home/kisaev/.bashrc
 source activate pairtree
 
 #set up input files
-input_files=/cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/Pairtree/2021-06-24_input_files
+input_files=/cluster/projects/kridelgroup/RAP_ANALYSIS/ANALYSIS/Pairtree/2021-09-26_input_files
 cd $input_files
 
 #don't forget to manually edit input *params.json files to remove the "" in the garbage []
@@ -31,7 +31,7 @@ cd min100_muts
 #nano  $input_files/p001_input.params.json to remove "" from garabge variable
 
 # Run Pairtree.
-$PTDIR/bin/pairtree --params $input_files/p001_input.params.json $input_files/p001_ssm_input.ssm p001.results.npz --seed 123 --trees-per-chain 4000 
+$PTDIR/bin/pairtree --params $input_files/p001_input.params.json $input_files/p001_ssm_input.ssm p001.results.npz --seed 123 --trees-per-chain 4000
 
 # Plot best tree results in an HTML file.
 $PTDIR/bin/plottree --runid p001 $input_files/p001_ssm_input.ssm $input_files/p001_input.params.json p001.results.npz p001.results.html --reorder-subclones
@@ -46,13 +46,13 @@ $PTDIR/bin/summposterior --runid p001 $input_files/p001_ssm_input.ssm $input_fil
 #nano  $input_files/p002_input.params.json to remove "" from garabge variable
 
 # Run Pairtree.
-$PTDIR/bin/pairtree --params $input_files/p002_input.params.json $input_files/p002_ssm_input.ssm p002.results.npz --seed 123 --trees-per-chain 4000
+#$PTDIR/bin/pairtree --params $input_files/p002_input.params.json $input_files/p002_ssm_input.ssm p002.results.npz --seed 123 --trees-per-chain 4000
 
 # Plot best tree results in an HTML file.
-$PTDIR/bin/plottree --runid p002 $input_files/p002_ssm_input.ssm $input_files/p002_input.params.json p002.results.npz p002.results.html --reorder-subclones
+#$PTDIR/bin/plottree --runid p002 $input_files/p002_ssm_input.ssm $input_files/p002_input.params.json p002.results.npz p002.results.html --reorder-subclones
 
 #plot all posteriors
-$PTDIR/bin/summposterior --runid p002 $input_files/p002_ssm_input.ssm $input_files/p002_input.params.json p002.results.npz p002.posterior_plots.html
+#$PTDIR/bin/summposterior --runid p002 $input_files/p002_ssm_input.ssm $input_files/p002_input.params.json p002.results.npz p002.posterior_plots.html
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #P003+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -61,10 +61,10 @@ $PTDIR/bin/summposterior --runid p002 $input_files/p002_ssm_input.ssm $input_fil
 #nano  $input_files/p003_input.params.json to remove "" from garabge variable
 
 # Run Pairtree.
-$PTDIR/bin/pairtree --params $input_files/p003_input.params.json $input_files/p003_ssm_input.ssm p003.results.npz --seed 123 --trees-per-chain 4000
+#$PTDIR/bin/pairtree --params $input_files/p003_input.params.json $input_files/p003_ssm_input.ssm p003.results.npz --seed 123 --trees-per-chain 4000
 
 # Plot best tree results in an HTML file.
-$PTDIR/bin/plottree --runid p003 $input_files/p003_ssm_input.ssm $input_files/p003_input.params.json p003.results.npz p003.results.html --reorder-subclones
+#$PTDIR/bin/plottree --runid p003 $input_files/p003_ssm_input.ssm $input_files/p003_input.params.json p003.results.npz p003.results.html --reorder-subclones
 
 #plot all posteriors
-$PTDIR/bin/summposterior --runid p003 $input_files/p003_ssm_input.ssm $input_files/p003_input.params.json p003.results.npz p003.posterior_plots.html
+#$PTDIR/bin/summposterior --runid p003 $input_files/p003_ssm_input.ssm $input_files/p003_input.params.json p003.results.npz p003.posterior_plots.html
